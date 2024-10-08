bfeb8c29d46285ab3e8e71c8471653f153d34982
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-08 09:04:23 -0400

    task 2.1

--- Lab6.js
+++ Lab6.js
@@ -5,15 +5,21 @@ let myString = "Hello, JavaScript!";
 console.log(myNumber);
 console.log(myString);
 
-//Task2
-for (let i = 0; i < 10; i++) {
-    if (i < 5) {
+//Task2.1
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+//Task2.2
+for (let i = 1; i < 11; i++) {
+    if (i < 6) {
         console.log(i * 3);
     } else {
         console.log(i);
     }
-
 }
+
+
 
 //Task3
 function addNumbers(num1, num2) {
@@ -23,8 +29,8 @@ function addNumbers(num1, num2) {
 console.log(addNumbers(20, 32));
 
 function multiplyNumbers(num1, num2) {
-    return num1*num2;
+    return num1 * num2;
 }
 
-console.log(multiplyNumbers(12,26));
+console.log(multiplyNumbers(12, 26));
 
