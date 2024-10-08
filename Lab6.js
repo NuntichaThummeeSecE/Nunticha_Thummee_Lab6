fb1f8e80b76d584995fe84e1dda8baa8185e1d56
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-08 08:58:26 -0400

    lab6.js

---
+++ Lab6.js
@@ -0,0 +1,19 @@
+//Task1
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+
+console.log(myNumber);
+console.log(myString);
+
+//Task2
+for (let i = 0; i < 10; i++) {
+    if (i < 5) {
+        console.log(i * 3);
+    } else {
+        console.log(i);
+    }
+
+}
+
+
+
