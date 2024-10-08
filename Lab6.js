66193c58611e721dc804f2938abb48aaab68994e
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-08 09:01:06 -0400

    task3

--- Lab6.js
+++ Lab6.js
@@ -15,5 +15,16 @@ for (let i = 0; i < 10; i++) {
 
 }
 
+//Task3
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
 
+console.log(addNumbers(20, 32));
+
+function multiplyNumbers(num1, num2) {
+    return num1*num2;
+}
+
+console.log(multiplyNumbers(12,26));
 
